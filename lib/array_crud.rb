e867b9dc47b89eddcd98043387ca23914be1e4c1
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 19:22:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- add_element_to_end_of_array << "sir"
+ array << "sir"
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_end_of_array.unshift("Pleasesir")
+  array.unshift ("Pleasesir")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop 
 end
 
 def remove_element_from_start_of_array(array)
